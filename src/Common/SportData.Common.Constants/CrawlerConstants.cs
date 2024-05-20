08d43eeb031acc5f321eb54c95b6bdfc5bc227a8
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-05-20 06:59:09 +0300

    Added projects - Constants, Exceptions and Extensions.

---
+++ src/Common/SportData.Common.Constants/CrawlerConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace SportData.Common.Constants;
+
+public static class CrawlerConstants
+{
+    public const string WORLD_COUNTRIES_MAIN_URL = "CrawlerSites:WorldCountries:MainUrl";
+    public const string WORLD_COUNTRIES_URL = "CrawlerSites:WorldCountries:CountriesUrl";
+    public const string WORLD_COUNTRIES_DOWNLOAD_IMAGE = "CrawlerSites:WorldCountries:ImageUrl";
+
+    public const string OLYMPEDIA_MAIN_URL = "CrawlerSites:Olympedia:MainUrl";
+    public const string OLYMPEDIA_NOC_URL = "CrawlerSites:Olympedia:NOCUrl";
+    public const string OLYMPEDIA_GAMES_URL = "CrawlerSites:Olympedia:GamesUrl";
+    public const string OLYMPEDIA_SPORTS_URL = "CrawlerSites:Olympedia:SportsUrl";
+    public const string OLYMPEDIA_VENUES_ULR = "CrawlerSites:Olympedia:VenuesUrl";
+}
---
+++ src/Common/SportData.Common.Constants/GlobalConstants.cs
@@ -0,0 +1,29 @@
+﻿namespace SportData.Common.Constants;
+
+public static class GlobalConstants
+{
+    public const string LOGGING = "Logging";
+    public const string LOG4NET_CORE = "Log4NetCore";
+
+    public const string APP_SETTINGS_FILE = "appsettings.json";
+
+    public const string CRAWLER_STORAGE_CONNECTION_STRING = "CrawlerStorageConnectionString";
+    public const string OLYMPIC_GAMES_CONNECTION_STRING = "OlympicGamesConnectionString";
+    public const string SPORT_DATA_CONNECTION_STRING = "SportDataConnectionString";
+
+    public const string AUTOMAPPER_MODELS_ASSEMBLY = "SportData.Data.Models";
+    public const string AUTOMAPPER_VIEW_MODELS_ASSEMBLY = "SportData.Data.ViewModels";
+
+    public const string JWT_KEY = "Jwt:Key";
+    public const string JWT_ISSUER = "Jwt:Issuer";
+    public const string JWT_AUDIENCE = "Jwt:Audience";
+    public const string JWT_TOKEN_VALIDITY_IN_MINUTES = "Jwt:TokenValidityInMinutes";
+    public const string JWT_REFRESH_TOKEN_VALIDITY_IN_DAYS = "Jwt:RefreshTokenValidityInDays";
+
+    public const string ALLOWED_ORIGINS = "AllowedOrigins";
+    public const string API_CORS = "ApiCors";
+    public const string BEARER = "Bearer";
+
+    public const double INCHES = 2.54;
+    public const double POUNDS = 2.20462;
+}
