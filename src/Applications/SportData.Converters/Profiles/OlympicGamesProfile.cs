501c1ead4b7c549e91dadbeb55c202bec3176af7
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-05-22 08:53:09 +0300

    Created new map.

--- src/Applications/SportData.Converters/Profiles/OlympicGamesProfile.cs
+++ src/Applications/SportData.Converters/Profiles/OlympicGamesProfile.cs
@@ -170,5 +170,37 @@ public class OlympicGamesProfile : Profile
             .ForPath(x => x.Team2.MatchResult, opt => opt.MapFrom(y => y.Team2.MatchResult))
             .ForPath(x => x.Team2.Points, opt => opt.MapFrom(y => y.Team2.Points))
             .ReverseMap();
+
+        this.CreateMap<MatchModel, AthleteMatch<Fencing>>()
+            .ForPath(x => x.Athlete1.Id, opt => opt.MapFrom(y => y.Team1.Id))
+            .ForPath(x => x.Athlete1.Name, opt => opt.MapFrom(y => y.Team1.Name))
+            .ForPath(x => x.Athlete1.NOC, opt => opt.MapFrom(y => y.Team1.NOC))
+            .ForPath(x => x.Athlete1.MatchResult, opt => opt.MapFrom(y => y.Team1.MatchResult))
+            .ForPath(x => x.Athlete1.Points, opt => opt.MapFrom(y => y.Team1.Points))
+            .ForPath(x => x.Athlete1.Seed, opt => opt.MapFrom(y => y.Team1.Seed))
+            .ForPath(x => x.Athlete1.Code, opt => opt.MapFrom(y => y.Team1.Code))
+            .ForPath(x => x.Athlete2.Id, opt => opt.MapFrom(y => y.Team2.Id))
+            .ForPath(x => x.Athlete2.Name, opt => opt.MapFrom(y => y.Team2.Name))
+            .ForPath(x => x.Athlete2.NOC, opt => opt.MapFrom(y => y.Team2.NOC))
+            .ForPath(x => x.Athlete2.MatchResult, opt => opt.MapFrom(y => y.Team2.MatchResult))
+            .ForPath(x => x.Athlete2.Points, opt => opt.MapFrom(y => y.Team2.Points))
+            .ForPath(x => x.Athlete2.Seed, opt => opt.MapFrom(y => y.Team2.Seed))
+            .ForPath(x => x.Athlete2.Code, opt => opt.MapFrom(y => y.Team2.Code))
+            .ReverseMap();
+
+        this.CreateMap<MatchModel, TeamMatch<Fencing>>()
+           .ForPath(x => x.Team1.Id, opt => opt.MapFrom(y => y.Team1.Id))
+           .ForPath(x => x.Team1.Name, opt => opt.MapFrom(y => y.Team1.Name))
+           .ForPath(x => x.Team1.NOC, opt => opt.MapFrom(y => y.Team1.NOC))
+           .ForPath(x => x.Team1.MatchResult, opt => opt.MapFrom(y => y.Team1.MatchResult))
+           .ForPath(x => x.Team1.Points, opt => opt.MapFrom(y => y.Team1.Points))
+           .ForPath(x => x.Team1.Seed, opt => opt.MapFrom(y => y.Team1.Seed))
+           .ForPath(x => x.Team2.Id, opt => opt.MapFrom(y => y.Team2.Id))
+           .ForPath(x => x.Team2.Name, opt => opt.MapFrom(y => y.Team2.Name))
+           .ForPath(x => x.Team2.NOC, opt => opt.MapFrom(y => y.Team2.NOC))
+           .ForPath(x => x.Team2.MatchResult, opt => opt.MapFrom(y => y.Team2.MatchResult))
+           .ForPath(x => x.Team2.Points, opt => opt.MapFrom(y => y.Team2.Points))
+           .ForPath(x => x.Team2.Seed, opt => opt.MapFrom(y => y.Team2.Seed))
+           .ReverseMap();
     }
 }
