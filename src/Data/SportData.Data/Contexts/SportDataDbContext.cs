e7062a1c4a6c27ec587f06b67612a8b654cdf772
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-06-24 15:12:53 +0300

    Added db contexts.

---
+++ src/Data/SportData.Data/Contexts/SportDataDbContext.cs
@@ -0,0 +1,59 @@
+﻿namespace SportData.Data.Contexts;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using SportData.Data.Common.Interfaces;
+using SportData.Data.Models.Entities.SportData;
+
+public class SportDataDbContext : IdentityDbContext<User, Role, string>
+{
+    public SportDataDbContext(DbContextOptions<SportDataDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public override int SaveChanges()
+    {
+        return this.SaveChanges(true);
+    }
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+        this.ApplyCheckRules();
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return this.SaveChangesAsync(true, cancellationToken);
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        this.ApplyCheckRules();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    private void ApplyCheckRules()
+    {
+        var changedEntries = this.ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is ICreatableEntity && (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+        foreach (var entry in changedEntries)
+        {
+            var entity = (ICreatableEntity)entry.Entity;
+            if (entry.State == EntityState.Added && entity.CreatedOn == default)
+            {
+                entity.CreatedOn = DateTime.UtcNow;
+            }
+            else
+            {
+                entity.ModifiedOn = DateTime.UtcNow;
+            }
+        }
+    }
+}
