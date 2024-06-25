42081d04c26bd86914e34012114a2247dfeb3d84
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-06-25 15:10:01 +0300

    Changed view models names.

--- src/Data/SportData.Data.ViewModels/Users/LoginViewModel.cs
+++ src/Data/SportData.Data.ViewModels/Users/LoginViewModel.cs
@@ -2,15 +2,11 @@
 
 using System.ComponentModel.DataAnnotations;
 
-public class RegistrationModel
+public class LoginViewModel
 {
     [Required(ErrorMessage = "Username is required!")]
     public string Username { get; set; }
 
     [Required(ErrorMessage = "Password is required!")]
     public string Password { get; set; }
-
-    [EmailAddress]
-    [Required(ErrorMessage = "Email is required!")]
-    public string Email { get; set; }
 }
--- src/Data/SportData.Data.ViewModels/Users/RegisterViewModel.cs
+++ src/Data/SportData.Data.ViewModels/Users/RegisterViewModel.cs
@@ -2,11 +2,15 @@
 
 using System.ComponentModel.DataAnnotations;
 
-public class LoginModel
+public class RegisterViewModel
 {
     [Required(ErrorMessage = "Username is required!")]
     public string Username { get; set; }
 
     [Required(ErrorMessage = "Password is required!")]
     public string Password { get; set; }
+
+    [EmailAddress]
+    [Required(ErrorMessage = "Email is required!")]
+    public string Email { get; set; }
 }
