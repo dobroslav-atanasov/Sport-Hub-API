a336564db8f7a3824e4ebe0aafe4bcc005c84b71
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-01 07:55:15 +0300

    Added messages.

--- src/Common/SportHub.Common.Constants/MessageConstants.cs
+++ src/Common/SportHub.Common.Constants/MessageConstants.cs
@@ -3,4 +3,6 @@
 public static class MessageConstants
 {
     public static string INVALID_USERNAME_OR_PASSWORD = "Invalid username or password.";
+    public static string USER_ALREADY_EXIST = "User already exist.";
+    public static string USER_CREATED_SUCCESSFULLY = "User created successfully.";
 }
