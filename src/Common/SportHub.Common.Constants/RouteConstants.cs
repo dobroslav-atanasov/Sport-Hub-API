d0d89e01da6ee40b392a31c13112ac6f7d92e2d7
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-09 23:05:00 +0300

    Added constants.

--- src/Common/SportHub.Common.Constants/RouteConstants.cs
+++ src/Common/SportHub.Common.Constants/RouteConstants.cs
@@ -3,7 +3,10 @@
 public static class RouteConstants
 {
     public const string API_DEFAULT_ROUTE = "/api/v{v:apiVersion}/[controller]";
+    public const string API_OLYMPIC_GAMES_AREA_ROUTE = "/api/v{v:apiVersion}/OlympicGames/[controller]";
 
     public const string TOKENS_CREATE_REFRESH_TOKEN = "create-refresh-token";
     public const string TOKENS_DELETE_REFRESH_TOKENS = "delete-refresh-tokens";
+
+    public const string OLYMPIC_GAMES_GAMES_ALL = "all";
 }
