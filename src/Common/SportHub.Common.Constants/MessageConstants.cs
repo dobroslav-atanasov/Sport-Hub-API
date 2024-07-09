4790ceb79660aa5da46002884be529479f67665d
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-09 23:04:37 +0300

    Added constants.

--- src/Common/SportHub.Common.Constants/MessageConstants.cs
+++ src/Common/SportHub.Common.Constants/MessageConstants.cs
@@ -2,7 +2,12 @@
 
 public static class MessageConstants
 {
+    public static string INVALID_USERNAME = "Invalid username.";
     public static string INVALID_USERNAME_OR_PASSWORD = "Invalid username or password.";
     public static string USER_ALREADY_EXIST = "User already exist.";
     public static string USER_CREATED_SUCCESSFULLY = "User created successfully.";
+    public static string INVALID_TOKEN = "Invalid token.";
+    public static string INVALID_ACCESS_TOKEN_OR_REFRESH_TOKEN = "Invalid access token or refresh token.";
+    public static string REFRESH_TOKEN_REVOKED = "Refresh token was revoked.";
+    public static string REFRESH_TOKEN_REVOKED_ALL_USERS = "Refresh token revoked for all users.";
 }
