e529385ade594159495156be066161b6d94ee578
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-11 09:20:55 +0300

    Added models.

---
+++ src/Data/SportHub.Data.Models/DbEntities/OlympicGames/AdditionalTables/GameType.cs
@@ -0,0 +1,8 @@
+﻿namespace SportHub.Data.Models.DbEntities.OlympicGames.AdditionalTables;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("GameTypes", Schema = "dbo")]
+public class GameType : BaseTable<int>
+{
+}
---
+++ src/Data/SportHub.Data.Models/DbEntities/OlympicGames/City.cs
@@ -0,0 +1,19 @@
+﻿namespace SportHub.Data.Models.DbEntities.OlympicGames;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using SportHub.Data.Common.Models;
+
+[Table("Cities", Schema = "dbo")]
+public class City : BaseDeletableEntity<Guid>
+{
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; }
+
+    public Guid CountryId { get; set; }
+    public virtual Country Country { get; set; }
+
+    public virtual ICollection<Game> Games { get; set; } = [];
+}
