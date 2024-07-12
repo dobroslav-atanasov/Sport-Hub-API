7e57895a94f19f9a6bbcc751673efe644efbd5b1
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-12 09:33:42 +0300

    Created area with controllers.

---
+++ src/Web/SportHub.WebAPI/Areas/OlympicGames/Controllers/GamesController.cs
@@ -0,0 +1,38 @@
+﻿namespace SportHub.WebAPI.Areas.OlympicGames.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+using SportHub.Common.Constants;
+using SportHub.Services.Data.OlympicGamesDb.Interfaces;
+
+public class GamesController : OlympicaGamesBaseController
+{
+    private readonly ILogger<GamesController> logger;
+    private readonly IGamesService gamesService;
+    private readonly ISportsService sportsService;
+
+    public GamesController(ILogger<GamesController> logger, IGamesService gamesService, ISportsService sportsService)
+    {
+        this.logger = logger;
+        this.gamesService = gamesService;
+        this.sportsService = sportsService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var game = await this.gamesService.GetGameAsync(id);
+        var sports = this.sportsService.GetSports();
+        game.Sports = sports;
+
+        return this.Ok(game);
+    }
+
+    [HttpGet]
+    [Route(RouteConstants.OLYMPIC_GAMES_GAMES_ALL)]
+    public IActionResult All()
+    {
+        var games = this.gamesService.GetGames();
+        return this.Ok(games);
+    }
+}
---
+++ src/Web/SportHub.WebAPI/Areas/OlympicGames/Controllers/OlympicaGamesBaseController.cs
@@ -0,0 +1,12 @@
+﻿namespace SportHub.WebAPI.Areas.OlympicGames.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+using SportHub.Common.Constants;
+using SportHub.WebAPI.Controllers;
+
+[Area(AreaConstants.AREA_OLYMPIC_GAMES)]
+[Route(RouteConstants.API_OLYMPIC_GAMES_AREA_ROUTE)]
+public abstract class OlympicaGamesBaseController : BaseController
+{
+}
