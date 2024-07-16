fe9197b1eb481ff0ff52d1a7c78ae1a43e47389a
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-16 15:09:57 +0300

    Removed entities.

--- src/Data/SportHub.Data.Models/DbEntities/OlympicGames/AdditionalTables/EventGender.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SportHub.Data.Models.DbEntities.OlympicGames.AdditionalTables;
-
-using System.ComponentModel.DataAnnotations.Schema;
-
-[Table("EventGenders", Schema = "dbo")]
-public class EventGender : BaseTable<int>
-{
-}
--- src/Data/SportHub.Data.Models/DbEntities/OlympicGames/AdditionalTables/GameType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SportHub.Data.Models.DbEntities.OlympicGames.AdditionalTables;
-
-using System.ComponentModel.DataAnnotations.Schema;
-
-[Table("GameTypes", Schema = "dbo")]
-public class GameType : BaseTable<int>
-{
-}
