0b8ca2fea002e56176e958e72d9b18d1130773f0
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-18 09:24:08 +0300

    Created models.

---
+++ src/Data/SportHub.Data.Models/Converters/Country.cs
@@ -0,0 +1,34 @@
+﻿namespace SportHub.Data.Models.Converters;
+
+public class Country
+{
+    public string Name { get; set; }
+
+    public string OfficialName { get; set; }
+
+    public bool IsIndependent { get; set; } = false;
+
+    public string TwoDigitsCode { get; set; }
+
+    public string Code { get; set; }
+
+    public string Capital { get; set; }
+
+    public string Continent { get; set; }
+
+    public string MemberOf { get; set; }
+
+    public int Population { get; set; }
+
+    public int TotalArea { get; set; }
+
+    public string HighestPointPlace { get; set; }
+
+    public int? HighestPoint { get; set; }
+
+    public string LowestPointPlace { get; set; }
+
+    public int? LowestPoint { get; set; }
+
+    public byte[] Flag { get; set; }
+}
---
+++ src/Data/SportHub.Data.Models/Converters/OlympicGames/ConverterModel.cs
@@ -0,0 +1,47 @@
+﻿namespace SportHub.Data.Models.Converters.OlympicGames;
+
+using HtmlAgilityPack;
+
+using SportHub.Data.Models.Cache;
+using SportHub.Data.Models.Enumerations.OlympicGames;
+
+public class ConverterModel
+{
+    public HtmlDocument HtmlDocument { get; set; }
+
+    public int PageId { get; set; }
+
+    public string Title { get; set; }
+
+    public Header Header { get; set; } = new Header();
+
+    public GameCache GameCache { get; set; }
+
+    public DisciplineCache DisciplineCache { get; set; }
+
+    public EventCache EventCache { get; set; }
+
+    public EventInfo EventInfo { get; set; } = new EventInfo();
+}
+
+public class Header
+{
+    public GameTypeEnum GameType { get; set; }
+
+    public int GameYear { get; set; }
+
+    public string Discipline { get; set; }
+
+    public string Event { get; set; }
+}
+
+public class EventInfo
+{
+    public string OriginalName { get; set; }
+
+    public string Name { get; set; }
+
+    public string NormalizedName { get; set; }
+
+    public string AdditionalInfo { get; set; }
+}
