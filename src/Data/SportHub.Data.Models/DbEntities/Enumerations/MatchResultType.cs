0ed551794ee6f6d87c643df45d5c25807077e766
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-18 18:55:15 +0300

    Replaced entities to new folder.

---
+++ src/Data/SportHub.Data.Models/DbEntities/Enumerations/MatchResultType.cs
@@ -0,0 +1,9 @@
+﻿namespace SportHub.Data.Models.DbEntities.Enumerations;
+
+public enum MatchResultType
+{
+    None = 1,
+    Win,
+    Lose,
+    Draw
+}
---
+++ src/Data/SportHub.Data.Models/DbEntities/SportHub/Role.cs
@@ -0,0 +1,14 @@
+﻿namespace SportHub.Data.Models.DbEntities.SportHub;
+
+using System;
+
+using global::SportHub.Data.Common.Interfaces;
+
+using Microsoft.AspNetCore.Identity;
+
+public class Role : IdentityRole, ICreatableEntity
+{
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+}
