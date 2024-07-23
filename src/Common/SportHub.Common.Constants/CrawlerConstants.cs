82d80a2d71202953465246e09805e6f3eb13d128
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-23 07:01:18 +0300

    Added constants.

--- src/Common/SportHub.Common.Constants/CrawlerConstants.cs
+++ src/Common/SportHub.Common.Constants/CrawlerConstants.cs
@@ -11,4 +11,18 @@ public static class CrawlerConstants
     public const string OLYMPEDIA_GAMES_URL = "CrawlerSites:Olympedia:GamesUrl";
     public const string OLYMPEDIA_SPORTS_URL = "CrawlerSites:Olympedia:SportsUrl";
     public const string OLYMPEDIA_VENUES_ULR = "CrawlerSites:Olympedia:VenuesUrl";
+
+    public const string PARIS_2024_LABELS_URL = "CrawlerSites:Paris2024:LabelsUrl";
+    public const string PARIS_2024_NOCS_URL = "CrawlerSites:Paris2024:NOCsUrl";
+    public const string PARIS_2024_NOC_URL = "CrawlerSites:Paris2024:NOCUrl";
+    public const string PARIS_2024_LINKS_URL = "CrawlerSites:Paris2024:LinksUrl";
+    public const string PARIS_2024_SPORT_CODES_URL = "CrawlerSites:Paris2024:SportCodesUrl";
+    public const string PARIS_2024_EVENT_CODES_URL = "CrawlerSites:Paris2024:EventCodesUrl";
+    public const string PARIS_2024_DISCIPLINES_URL = "CrawlerSites:Paris2024:DisciplinesUrl";
+    public const string PARIS_2024_TEAMS_URL = "CrawlerSites:Paris2024:TeamsUrl";
+    public const string PARIS_2024_HORSES_URL = "CrawlerSites:Paris2024:HorsesUrl";
+    public const string PARIS_2024_PARTICIPATIONS_URL = "CrawlerSites:Paris2024:ParticipationsUrl";
+    public const string PARIS_2024_ATHLETES_URL = "CrawlerSites:Paris2024:AthletesUrl";
+    public const string PARIS_2024_COACHES_URL = "CrawlerSites:Paris2024:CoachesUrl";
+    public const string PARIS_2024_JUDGES_URL = "CrawlerSites:Paris2024:JudgesUrl";
 }
