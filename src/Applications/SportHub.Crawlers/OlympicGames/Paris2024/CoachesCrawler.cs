db1b01d696bd81e80bcf62bc80497e08947fa572
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-24 09:34:10 +0300

    Created crawlers.

---
+++ src/Applications/SportHub.Crawlers/OlympicGames/Paris2024/CoachesCrawler.cs
@@ -0,0 +1,35 @@
+﻿namespace SportHub.Crawlers.OlympicGames.Paris2024;
+
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using SportHub.Common.Constants;
+using SportHub.Services.Data.CrawlerStorageDb.Interfaces;
+using SportHub.Services.Interfaces;
+
+public class CoachesCrawler : BaseCrawler
+{
+    public CoachesCrawler(ILogger<BaseCrawler> logger, IConfiguration configuration, IHttpService httpService, ICrawlersService crawlersService, IGroupsService groupsService)
+        : base(logger, configuration, httpService, crawlersService, groupsService)
+    {
+    }
+
+    public override async Task StartAsync()
+    {
+        this.Logger.LogInformation($"{this.GetType().FullName} Start!");
+
+        try
+        {
+            var httpModel = await this.HttpService.GetAsync(this.Configuration.GetSection(CrawlerConstants.PARIS_2024_COACHES_URL).Value);
+            await this.ProcessGroupAsync(httpModel);
+        }
+        catch (Exception ex)
+        {
+            this.Logger.LogError(ex, $"Failed to process url: {this.Configuration.GetSection(CrawlerConstants.PARIS_2024_COACHES_URL).Value};");
+        }
+
+        this.Logger.LogInformation($"{this.GetType().FullName} End!");
+    }
+}
---
+++ src/Applications/SportHub.Crawlers/OlympicGames/Paris2024/NOCCrawler.cs
@@ -0,0 +1,53 @@
+﻿namespace SportHub.Crawlers.OlympicGames.Paris2024;
+
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using SportHub.Common.Constants;
+using SportHub.Data.Models.Crawlers.Paris2024.NOCs;
+using SportHub.Services.Data.CrawlerStorageDb.Interfaces;
+using SportHub.Services.Interfaces;
+
+public class NOCCrawler : BaseCrawler
+{
+    public NOCCrawler(ILogger<BaseCrawler> logger, IConfiguration configuration, IHttpService httpService, ICrawlersService crawlersService, IGroupsService groupsService)
+        : base(logger, configuration, httpService, crawlersService, groupsService)
+    {
+    }
+
+    public override async Task StartAsync()
+    {
+        this.Logger.LogInformation($"{this.GetType().FullName} Start!");
+
+        try
+        {
+            var httpModel = await this.HttpService.GetAsync(this.Configuration.GetSection(CrawlerConstants.PARIS_2024_NOCS_URL).Value);
+            var model = JsonSerializer.Deserialize<NOCList>(httpModel.Content);
+
+            foreach (var noc in model.NOCs)
+            {
+                var url = $"{this.Configuration.GetSection(CrawlerConstants.PARIS_2024_NOC_URL).Value}{noc.Code}.json";
+
+                try
+                {
+                    var nocHttpModel = await this.HttpService.GetAsync(url);
+                    await this.ProcessGroupAsync(nocHttpModel);
+                }
+                catch (Exception ex)
+                {
+                    this.Logger.LogError(ex, $"Failed to process url: {url};");
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            this.Logger.LogError(ex, $"Failed to process url: {this.Configuration.GetSection(CrawlerConstants.PARIS_2024_NOCS_URL).Value};");
+        }
+
+        this.Logger.LogInformation($"{this.GetType().FullName} End!");
+    }
+}
