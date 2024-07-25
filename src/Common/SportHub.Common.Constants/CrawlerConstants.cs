3bb577a55b852dccb3d064477b9d94f067df6366
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-07-25 09:46:38 +0300

    Added constants.

--- src/Common/SportHub.Common.Constants/CrawlerConstants.cs
+++ src/Common/SportHub.Common.Constants/CrawlerConstants.cs
@@ -25,4 +25,26 @@ public static class CrawlerConstants
     public const string PARIS_2024_ATHLETES_URL = "CrawlerSites:Paris2024:AthletesUrl";
     public const string PARIS_2024_COACHES_URL = "CrawlerSites:Paris2024:CoachesUrl";
     public const string PARIS_2024_JUDGES_URL = "CrawlerSites:Paris2024:JudgesUrl";
+    public const string PARIS_2024_ATHLETE_URL = "CrawlerSites:Paris2024:AthleteUrl";
+    public const string PARIS_2024_ATHLETE_IMAGE_URL = "CrawlerSites:Paris2024:AthleteImageUrl";
+    public const string PARIS_2024_ATHLETE_PAGE_URL = "CrawlerSites:Paris2024:AthletePageUrl";
+    public const string PARIS_2024_MAIN_URL = "CrawlerSites:Paris2024:MainUrl";
+    public const string PARIS_2024_HORSE_URL = "CrawlerSites:Paris2024:HorseUrl";
+    public const string PARIS_2024_TEAM_URL = "CrawlerSites:Paris2024:TeamUrl";
+    public const string PARIS_2024_SCHEDULE_URL = "CrawlerSites:Paris2024:ScheduleUrl";
+    public const string PARIS_2024_DISCIPLINE_UNITS_URL = "CrawlerSites:Paris2024:DisciplineUnitsUrl";
+    public const string PARIS_2024_DISCIPLINE_IMAGE_URL = "CrawlerSites:Paris2024:DisciplineImageUrl";
+    public const string PARIS_2024_DISCIPLINE_POSITIONS_URL = "CrawlerSites:Paris2024:DisciplinePositionsUrl";
+    public const string PARIS_2024_DISCIPLINE_SCHEDULES_URL = "CrawlerSites:Paris2024:DisciplineSchedulesUrl";
+    public const string PARIS_2024_DISCIPLINE_EVENTS_URL = "CrawlerSites:Paris2024:DisciplineEventsUrl";
+    public const string PARIS_2024_DISCIPLINE_EVENT_UNITS_URL = "CrawlerSites:Paris2024:DisciplineEventUnitsUrl";
+    public const string PARIS_2024_DISCIPLINE_RECORDS_URL = "CrawlerSites:Paris2024:DisciplineRecordsUrl";
+    public const string PARIS_2024_DISCIPLINE_INFO_URL = "CrawlerSites:Paris2024:DisciplineInfoUrl";
+    public const string PARIS_2024_EVENT_SCHEDULE_URL = "CrawlerSites:Paris2024:EventScheduleUrl";
+    public const string PARIS_2024_EVENT_RANKING_URL = "CrawlerSites:Paris2024:EventRankingUrl";
+    public const string PARIS_2024_EVENT_PHASES_URL = "CrawlerSites:Paris2024:EventPhasesUrl";
+    public const string PARIS_2024_RESULT_URL = "CrawlerSites:Paris2024:ResultUrl";
+    public const string PARIS_2024_PDFS_URL = "CrawlerSites:Paris2024:PDFsUrl";
+    public const string PARIS_2024_PDF_URL = "CrawlerSites:Paris2024:PDFUrl";
+
 }
