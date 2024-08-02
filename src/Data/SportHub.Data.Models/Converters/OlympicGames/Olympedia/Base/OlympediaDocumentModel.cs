2de3f518fe339a8d3412ef65463802a164b6da5d
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-08-02 09:01:15 +0300

    Created base converter models.

---
+++ src/Data/SportHub.Data.Models/Converters/OlympicGames/Olympedia/Base/OlympediaDocumentModel.cs
@@ -0,0 +1,26 @@
+﻿namespace SportHub.Data.Models.Converters.OlympicGames.Olympedia.Base;
+
+using HtmlAgilityPack;
+
+using SportHub.Data.Models.Cache;
+
+public class OlympediaDocumentModel
+{
+    public HtmlDocument HtmlDocument { get; set; }
+
+    public int PageId { get; set; }
+
+    public string Title { get; set; }
+
+    public Header Header { get; set; } = new Header();
+
+    public GameCache GameCache { get; set; }
+
+    public DisciplineCache DisciplineCache { get; set; }
+
+    public EventCache EventCache { get; set; }
+
+    public EventInfo EventInfo { get; set; } = new EventInfo();
+
+    public bool IsValidEvent { get; set; } = true;
+}
---
+++ src/Data/SportHub.Data.Models/Converters/OlympicGames/Paris2024/Base/Paris2024ConverterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SportHub.Data.Models.Converters.OlympicGames.Paris2024.Base;
+
+using System.Text;
+
+public class Paris2024ConverterModel
+{
+    public Encoding Encoding { get; set; }
+
+    public string Json { get; set; }
+}
