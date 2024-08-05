4e0749f05764e5fe9697e8b433523fdf3bcaf5a3
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-08-05 08:03:36 +0300

    Created model.

---
+++ src/Data/SportHub.Data.Models/Converters/OlympicGames/CodeInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace SportHub.Data.Models.Converters.OlympicGames;
+
+public class CodeInfo
+{
+    public string GameType { get; set; }
+
+    public int Year { get; set; }
+
+    public string Discipline { get; set; }
+
+    public string Gender { get; set; }
+
+    public string Event { get; set; }
+}
