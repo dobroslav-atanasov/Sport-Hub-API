525d50a8c1bf982ca9b351b93c2b7bbcc1fba464
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-08-12 07:16:44 +0300

    Added models.

---
+++ src/Data/SportHub.Data.Models/Crawlers/Paris2024/Competitor.cs
@@ -0,0 +1,21 @@
+﻿namespace SportHub.Data.Models.Crawlers.Paris2024;
+
+using System.Text.Json.Serialization;
+
+public class Competitor
+{
+    [JsonPropertyName("code")]
+    public string Code { get; set; }
+
+    [JsonPropertyName("noc")]
+    public string Noc { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("order")]
+    public int Order { get; set; }
+
+    [JsonPropertyName("results")]
+    public Results Results { get; set; }
+}
---
+++ src/Data/SportHub.Data.Models/Jsons/OlympicGames/NOC.cs
@@ -0,0 +1,62 @@
+﻿namespace SportHub.Data.Models.Jsons.OlympicGames;
+
+public class NOC
+{
+    public int Id { get; set; }
+
+    public string Code { get; set; }
+
+    public bool IsHistoric { get; set; }
+
+    public bool IsMedal { get; set; }
+
+    public Membership Membership { get; set; }
+
+    public Interest Interest { get; set; }
+
+    public OpenCeremony OpenCeremony { get; set; }
+
+    public Participation Participation { get; set; }
+
+    //public Lis Oficials { get; set; }
+}
+
+public class Participation
+{
+    public int? FirstAppearence { get; set; }
+
+    public int? Appearance { get; set; }
+
+    public string Summary { get; set; }
+}
+
+public class OpenCeremony
+{
+    public List<string> FlagBearers { get; set; } = [];
+}
+
+public class Interest
+{
+    public string Highlights { get; set; }
+
+    public string Information { get; set; }
+}
+
+public class Membership
+{
+    public string Name { get; set; }
+
+    public string OfficialName { get; set; }
+
+    public string OfficialCommitteeName { get; set; }
+
+    public string SEOName { get; set; }
+
+    public int? FoundingDate { get; set; }
+
+    public int? IOCRecognitionDate { get; set; }
+
+    public int? DisbandedDate { get; set; }
+
+    public string RelatedNOC { get; set; }
+}
