178fd853e5e77323867a8e4237b033ae9f5e28b4
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-08-15 08:27:18 +0300

    Created enumeration.

---
+++ src/Data/SportHub.Data.Models/Enumerations/OlympicGames/CodeEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace SportHub.Data.Models.Enumerations.OlympicGames;
+
+public enum CodeEnum
+{
+    GameType = 1,
+    Year,
+    Discipline,
+    Gender,
+    Event,
+    Phase,
+    Unit
+}
