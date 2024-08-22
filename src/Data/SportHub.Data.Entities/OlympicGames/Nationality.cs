31613e3e340d2ecb7e8433a862750f016e5194c0
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-08-22 10:08:49 +0300

    Added entities.

---
+++ src/Data/SportHub.Data.Entities/OlympicGames/Nationality.cs
@@ -0,0 +1,21 @@
+﻿namespace SportHub.Data.Entities.OlympicGames;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using global::SportHub.Data.Common.Models;
+
+[Table("Nationalities", Schema = "dbo")]
+public class Nationality : BaseDeletableEntity<int>
+{
+    [MaxLength(10)]
+    public string Code { get; set; }
+
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MaxLength(100)]
+    public string LongName { get; set; }
+
+    public virtual ICollection<Person> Persons { get; set; } = new HashSet<Person>();
+}
---
+++ src/Data/SportHub.Data.Entities/OlympicGames/Participation.cs
@@ -0,0 +1,35 @@
+﻿namespace SportHub.Data.Entities.OlympicGames;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using global::SportHub.Data.Common.Models;
+
+[Table("Participations", Schema = "dbo")]
+public class Participation : BaseDeletableEntity<Guid>, IEquatable<Participation>
+{
+    [Required]
+    [MaxLength(20)]
+    public string Code { get; set; }
+
+    public Guid PersonId { get; set; }
+    public virtual Person Person { get; set; }
+
+    public Guid EventId { get; set; }
+    public virtual Event Event { get; set; }
+
+    public int NOCId { get; set; }
+    public virtual NOC NOC { get; set; }
+
+    public bool Equals(Participation other)
+    {
+        var equals = true;
+
+        return equals;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Participation && this.Equals((Participation)obj);
+    }
+}
