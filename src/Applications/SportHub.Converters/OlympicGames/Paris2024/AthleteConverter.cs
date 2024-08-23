39d2013e4cf707a05b2dfc42d499454afcf78d74
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-08-23 08:47:38 +0300

    Created converters.

---
+++ src/Applications/SportHub.Converters/OlympicGames/Paris2024/AthleteConverter.cs
@@ -0,0 +1,77 @@
+﻿namespace SportHub.Converters.OlympicGames.Paris2024;
+
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportHub.Data.Entities.Crawlers;
+using SportHub.Data.Entities.OlympicGames;
+using SportHub.Data.Models.Crawlers.Paris2024.Person;
+using SportHub.Data.Repositories;
+using SportHub.Services.Data.CrawlerStorageDb.Interfaces;
+using SportHub.Services.Data.OlympicGamesDb.Interfaces;
+using SportHub.Services.Interfaces;
+
+public class AthleteConverter : BaseConverter
+{
+    private readonly OlympicGamesRepository<Person> repository;
+
+    public AthleteConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
+        INormalizeService normalizeService, IDataCacheService dataCacheService, OlympicGamesRepository<Person> repository)
+        : base(logger, crawlersService, logsService, groupsService, zipService, normalizeService, dataCacheService)
+    {
+        this.repository = repository;
+    }
+
+    protected override async Task ProcessGroupAsync(Group group)
+    {
+        var converterModel = this.PrepareParis2024ConverterModel(group);
+        var model = JsonSerializer.Deserialize<PersonCrawlerModel>(converterModel.Paris2024Documents.GetValueOrDefault(1).Json);
+
+        var dbPerson = await this.repository.GetAsync(x => x.Code == model.Person.Code);
+        dbPerson.FirstName = model.Person.GivenName;
+        dbPerson.LastName = model.Person.FamilyName;
+        dbPerson.Weight = model.Person.Weight;
+        dbPerson.Height = dbPerson.Height == 0 ? model.Person.Height : 0;
+        dbPerson.Nationality = model.Person.Nationality?.Code;
+        dbPerson.PlaceOfBirth = model.Person.ParticipantBio?.PlaceofBirth;
+        dbPerson.CountryOfBirth = model.Person.ParticipantBio?.CountryofBirth?.Code;
+        dbPerson.Residence = model.Person.ParticipantBio?.PlaceofResidence;
+        dbPerson.CountryOfResidence = model.Person.ParticipantBio?.CountryofResidence?.Code;
+        dbPerson.Status = model.Person.Status?.Description;
+
+        if (model.Person.ParticipantBio != null)
+        {
+            if (model.Person.ParticipantBio.Highlights != null)
+            {
+                dbPerson.HighlightsType = string.Join("|", model.Person.ParticipantBio.Highlights.Select(x => x.Type));
+                dbPerson.Highlights = string.Join("|", model.Person.ParticipantBio.Highlights.Select(x => x.Value));
+            }
+
+            if (model.Person.ParticipantBio.Interest != null)
+            {
+                dbPerson.Nickname = model.Person.ParticipantBio.Interest.Nickname;
+                dbPerson.Hobbies = model.Person.ParticipantBio.Interest.Hobbies;
+                dbPerson.Occupation = model.Person.ParticipantBio.Interest.Occupation;
+                dbPerson.Education = model.Person.ParticipantBio.Interest.Education;
+                dbPerson.Family = model.Person.ParticipantBio.Interest.Family;
+                dbPerson.LanguagesSpoken = model.Person.ParticipantBio.Interest.LangSpoken;
+                dbPerson.Coach = model.Person.ParticipantBio.Interest.Coach;
+                dbPerson.Debut = model.Person.ParticipantBio.Interest.Debut;
+                dbPerson.Start = model.Person.ParticipantBio.Interest.Start;
+                dbPerson.Reason = model.Person.ParticipantBio.Interest.Reason;
+                dbPerson.Ambition = model.Person.ParticipantBio.Interest.Ambition;
+                dbPerson.Milestones = model.Person.ParticipantBio.Interest.Milestones;
+                dbPerson.Hero = model.Person.ParticipantBio.Interest.Hero;
+                dbPerson.Influence = model.Person.ParticipantBio.Interest.Influence;
+                dbPerson.Philosophy = model.Person.ParticipantBio.Interest.Philosophy;
+                dbPerson.Award = model.Person.ParticipantBio.Interest.Award;
+                dbPerson.AddInformation = model.Person.ParticipantBio.Interest.AddInformation;
+            }
+        }
+
+        this.repository.Update(dbPerson);
+        await this.repository.SaveChangesAsync();
+    }
+}
---
+++ src/Applications/SportHub.Converters/OlympicGames/Paris2024/ParticipantConverter.cs
@@ -0,0 +1,111 @@
+﻿namespace SportHub.Converters.OlympicGames.Paris2024;
+
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportHub.Data.Entities.Crawlers;
+using SportHub.Data.Entities.Enumerations.OlympicGames;
+using SportHub.Data.Entities.OlympicGames;
+using SportHub.Data.Models.Crawlers.Paris2024.Person;
+using SportHub.Services.Data.CrawlerStorageDb.Interfaces;
+using SportHub.Services.Data.OlympicGamesDb;
+using SportHub.Services.Data.OlympicGamesDb.Interfaces;
+using SportHub.Services.Interfaces;
+
+public class ParticipantConverter : BaseConverter
+{
+    private readonly IDataService<Participation> participationsService;
+    private readonly IDataService<Person> personsService;
+
+    public ParticipantConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
+        INormalizeService normalizeService, IDataCacheService dataCacheService, DataService<Participation> participationsService, DataService<Person> personsService)
+        : base(logger, crawlersService, logsService, groupsService, zipService, normalizeService, dataCacheService)
+    {
+        this.participationsService = participationsService;
+        this.personsService = personsService;
+    }
+
+    protected override async Task ProcessGroupAsync(Group group)
+    {
+        var converterModel = this.PrepareParis2024ConverterModel(group);
+        var model = JsonSerializer.Deserialize<PersonCrawlerModel>(converterModel.Paris2024Documents.GetValueOrDefault(1).Json);
+        var personJson = model.Person;
+
+        var medals = new Dictionary<string, int>();
+        if (personJson.Medals != null)
+        {
+            foreach (var medal in personJson.Medals)
+            {
+                var eventCode = this.GenerateCode(GameTypeEnum.Summer, 2024, medal.EventCode);
+
+                if (!medals.ContainsKey(eventCode))
+                {
+                    medals[eventCode] = int.Parse(medal.MedalType);
+                }
+            }
+        }
+
+        var person = await this.personsService.GetAsync(x => x.Code == personJson.Code);
+        var noc = this.DataCacheService.NOCs.FirstOrDefault(x => x.Code == personJson.Organisation.Code);
+        var game = this.DataCacheService.Games.FirstOrDefault(x => x.Year == 2024);
+        var age = this.CalculateAge(game.OpeningCeremony ?? game.StartCompetitionDate.Value, person.BirthDate.Value);
+
+        foreach (var regEvent in personJson.RegisteredEvents)
+        {
+            var eventCode = this.GenerateCode(GameTypeEnum.Summer, 2024, regEvent.Event.Code);
+            var @event = this.DataCacheService.Events.FirstOrDefault(x => x.Code == eventCode);
+
+            if (@event != null)
+            {
+                var participation = new Participation
+                {
+                    Code = personJson.Code,
+                    PersonId = person.Id,
+                    NOCId = noc.Id,
+                    EventId = @event.Id,
+                    AgeYears = age.Item1,
+                    AgeDays = age.Item2,
+                    Medal = medals.GetValueOrDefault(eventCode),
+                    Rank = regEvent.Event.Rankings?.FirstOrDefault(x => x.AthleteCode == person.Code)?.RkRank,
+                    RkRankEqual = regEvent.Event.Rankings?.FirstOrDefault(x => x.AthleteCode == person.Code)?.RkRankEqual,
+                    RkResultType = regEvent.Event.Rankings?.FirstOrDefault(x => x.AthleteCode == person.Code)?.RkResultType,
+                    RkType = regEvent.Event.Rankings?.FirstOrDefault(x => x.AthleteCode == person.Code)?.RkType,
+                };
+
+                var dbParticipation = await this.participationsService
+                    .GetAsync(x => x.PersonId == participation.PersonId && x.EventId == participation.EventId && x.NOCId == participation.NOCId);
+                if (dbParticipation != null)
+                {
+                    var equals = participation.Equals(dbParticipation);
+                    if (!equals)
+                    {
+                        this.participationsService.Update(dbParticipation);
+                    }
+                }
+                else
+                {
+                    await this.participationsService.AddAsync(participation);
+                }
+            }
+        }
+    }
+
+    private Tuple<int, int> CalculateAge(DateTime? startDate, DateTime? endDate)
+    {
+        var age = new Tuple<int, int>(0, 0);
+        if (!startDate.HasValue || !endDate.HasValue)
+        {
+            return age;
+        }
+
+        var totalDays = (startDate.Value - endDate.Value).TotalDays;
+        var year = (int)Math.Floor(totalDays / 365.25);
+        var newYear = endDate.Value.AddYears(year);
+        var days = (startDate.Value - newYear).TotalDays;
+        age = new Tuple<int, int>(year, (int)days);
+
+        return age;
+    }
+}
