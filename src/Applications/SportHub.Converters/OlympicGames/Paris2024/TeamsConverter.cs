6dbe7b3b2eb9d70073d7a911a80ccecf9dbc2f20
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-08-26 08:19:58 +0300

    Created entity, converter and json model.

---
+++ src/Applications/SportHub.Converters/OlympicGames/Paris2024/TeamsConverter.cs
@@ -0,0 +1,56 @@
+﻿namespace SportHub.Converters.OlympicGames.Paris2024;
+
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportHub.Converters.OlympicGames.Paris2024.Base;
+using SportHub.Data.Entities.Crawlers;
+using SportHub.Data.Entities.OlympicGames;
+using SportHub.Data.Models.Crawlers.Paris2024.Teams;
+using SportHub.Services.Data.CrawlerStorageDb.Interfaces;
+using SportHub.Services.Data.OlympicGamesDb;
+using SportHub.Services.Data.OlympicGamesDb.Interfaces;
+using SportHub.Services.Interfaces;
+
+public class TeamsConverter : Paris2024Converter
+{
+    private readonly DataService<Team> teamsService;
+
+    public TeamsConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
+        INormalizeService normalizeService, IDataCacheService dataCacheService, DataService<Team> teamsService)
+        : base(logger, crawlersService, logsService, groupsService, zipService, normalizeService, dataCacheService)
+    {
+        this.teamsService = teamsService;
+    }
+
+    protected override async Task ProcessGroupAsync(Group group)
+    {
+        var converterModel = this.PrepareConverterModel(group);
+        var model = JsonSerializer.Deserialize<TeamsCrawlerModel>(converterModel.Documents.GetValueOrDefault(1).Json);
+
+        var teams = new List<Team>();
+        foreach (var item in model.Teams.Where(x => x.Current))
+        {
+            // TODO Generate team code.
+
+            var team = new Team
+            {
+                Code = "1",
+                OriginalCode = item.Code,
+                Name = item.Name,
+                ShortName = item.ShortName,
+                TvName = item.Name,
+                Gender = item.TeamGender,
+                Type = item.TeamType,
+                Organisation = item.Organisation.Code
+            };
+
+
+            teams.Add(team);
+        }
+
+        await this.teamsService.AddRangeAsync(teams);
+    }
+}
---
+++ src/Data/SportHub.Data.Entities/OlympicGames/Team.cs
@@ -0,0 +1,58 @@
+﻿namespace SportHub.Data.Entities.OlympicGames;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using global::SportHub.Data.Common.Models;
+
+[Table("Teams", Schema = "dbo")]
+public class Team : BaseDeletableEntity<Guid>, IEquatable<Team>
+{
+    [Required]
+    [MaxLength(50)]
+    public string Code { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string TvName { get; set; }
+
+    [MaxLength(200)]
+    public string ShortName { get; set; }
+
+    [Required]
+    [StringLength(1)]
+    public string Gender { get; set; }
+
+    [MaxLength(50)]
+    public string Type { get; set; }
+
+    [MaxLength(10)]
+    public string Organisation { get; set; }
+
+    [MaxLength(50)]
+    public string OriginalCode { get; set; }
+
+    public virtual ICollection<Participation> Athletes { get; set; } = new HashSet<Participation>();
+
+    public bool Equals(Team other)
+    {
+        var equal = true;
+
+        //if (this.Order != other.Order)
+        //{
+        //    other.Order = this.Order;
+        //    equal = false;
+        //}
+
+        return equal;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Team && this.Equals((Team)obj);
+    }
+}
