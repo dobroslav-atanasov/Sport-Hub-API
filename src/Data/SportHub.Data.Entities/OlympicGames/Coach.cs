509f567880722bf005df3513815e0aa6a825071f
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-08-28 08:17:09 +0300

    Created entity.

---
+++ src/Data/SportHub.Data.Entities/OlympicGames/Coach.cs
@@ -0,0 +1,16 @@
+﻿namespace SportHub.Data.Entities.OlympicGames;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+using global::SportHub.Data.Common.Models;
+
+[Table("Coaches", Schema = "dbo")]
+public class Coach : BaseDeletableEntity<Guid>
+{
+    public Guid TeamId { get; set; }
+    public virtual Team Team { get; set; }
+
+    public Guid CoachId { get; set; }
+
+    public int Order { get; set; }
+}
