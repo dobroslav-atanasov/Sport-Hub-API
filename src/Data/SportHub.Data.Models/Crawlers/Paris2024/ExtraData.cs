145392fe6244436db3021c09d57d40d8c984619d
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-08-29 10:16:01 +0300

    Removed unnecessary crawler json models.

--- src/Data/SportHub.Data.Models/Crawlers/Paris2024/ExtraData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SportHub.Data.Models.Crawlers.Paris2024;
-
-using System.Text.Json.Serialization;
-
-public class ExtraData
-{
-    [JsonPropertyName("detailUrl")]
-    public string DetailUrl { get; set; }
-}
--- src/Data/SportHub.Data.Models/Crawlers/Paris2024/NOCs/NOCList.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SportHub.Data.Models.Crawlers.Paris2024.NOCs;
-
-using System.Text.Json.Serialization;
-
-public class NOCList
-{
-    [JsonPropertyName("nocs")]
-    public List<NOC> NOCs { get; set; }
-}
