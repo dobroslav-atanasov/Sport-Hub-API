1b0ce7d1356058173f6d89865f228f1c4f491307
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-08-29 10:17:18 +0300

    Updated crawler json model.

---
+++ src/Data/SportHub.Data.Models/Crawlers/Paris2024/NOCsCrawlerModel.cs
@@ -0,0 +1,39 @@
+﻿namespace SportHub.Data.Models.Crawlers.Paris2024.NOCs;
+
+using System.Text.Json.Serialization;
+
+public class NOCsCrawlerModel
+{
+    [JsonPropertyName("nocs")]
+    public List<NOCInfo> NOCs { get; set; }
+}
+
+public class NOCInfo
+{
+    [JsonPropertyName("code")]
+    public string Code { get; set; }
+
+    [JsonPropertyName("note")]
+    public string Note { get; set; }
+
+    [JsonPropertyName("medal")]
+    public string Medal { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("longDescription")]
+    public string LongDescription { get; set; }
+
+    [JsonPropertyName("seodescription")]
+    public string Seodescription { get; set; }
+
+    [JsonPropertyName("protocolOrder")]
+    public int ProtocolOrder { get; set; }
+
+    [JsonPropertyName("descriptionOrder")]
+    public int DescriptionOrder { get; set; }
+
+    [JsonPropertyName("longDescriptionOrder")]
+    public int LongDescriptionOrder { get; set; }
+}
