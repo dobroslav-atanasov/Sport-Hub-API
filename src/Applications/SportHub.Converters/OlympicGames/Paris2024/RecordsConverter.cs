eb6f0997f44f0e392d2d36403c7cabffb9edc2b6
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-09-02 11:01:10 +0300

    Added converters.

---
+++ src/Applications/SportHub.Converters/OlympicGames/Paris2024/RecordsConverter.cs
@@ -0,0 +1,50 @@
+﻿namespace SportHub.Converters.OlympicGames.Paris2024;
+
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportHub.Converters.OlympicGames.Paris2024.Base;
+using SportHub.Data.Entities.Crawlers;
+using SportHub.Data.Entities.OlympicGames;
+using SportHub.Data.Models.Crawlers.Paris2024.Records;
+using SportHub.Services.Data.CrawlerStorageDb.Interfaces;
+using SportHub.Services.Data.OlympicGamesDb.Interfaces;
+using SportHub.Services.Interfaces;
+
+public class RecordsConverter : Paris2024Converter
+{
+    public RecordsConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
+        INormalizeService normalizeService, IDataCacheService dataCacheService)
+        : base(logger, crawlersService, logsService, groupsService, zipService, normalizeService, dataCacheService)
+    {
+    }
+
+    protected override async Task ProcessGroupAsync(Group group)
+    {
+        var converterModel = this.PrepareConverterModel(group);
+        var json = JsonSerializer.Deserialize<RecordsCrawlerModel>(converterModel.Documents.GetValueOrDefault(6).Json);
+
+        if (json.Records != null)
+        {
+            foreach (var item in json.Records)
+            {
+                var code = this.GenerateCode("Summer", 2024, item.RecordCode);
+                if (item.RecordData.Count > 1)
+                {
+                    ;
+                }
+                var record = new Record
+                {
+                    Code = item.RecordId,
+                    NotEstablished = item.NotEstablished,
+                    Order = item.Order,
+                    Name = item.RecordType.Description,
+                    Type = item.RecordType.Code,
+                    Description = item.Record.Description,
+                };
+            }
+        }
+    }
+}
---
+++ src/Applications/SportHub.Converters/OlympicGames/Paris2024/ScheduleConverter.cs
@@ -0,0 +1,53 @@
+﻿namespace SportHub.Converters.OlympicGames.Paris2024;
+
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportHub.Converters.OlympicGames.Paris2024.Base;
+using SportHub.Data.Entities.Crawlers;
+using SportHub.Data.Entities.OlympicGames;
+using SportHub.Data.Models.Crawlers.Paris2024.UnitGroup;
+using SportHub.Services.Data.CrawlerStorageDb.Interfaces;
+using SportHub.Services.Data.OlympicGamesDb;
+using SportHub.Services.Data.OlympicGamesDb.Interfaces;
+using SportHub.Services.Interfaces;
+
+public class ScheduleConverter : Paris2024Converter
+{
+    private readonly DataService<Unit> unitsService;
+
+    public ScheduleConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
+        INormalizeService normalizeService, IDataCacheService dataCacheService, DataService<Unit> unitsService)
+        : base(logger, crawlersService, logsService, groupsService, zipService, normalizeService, dataCacheService)
+    {
+        this.unitsService = unitsService;
+    }
+
+    protected override async Task ProcessGroupAsync(Group group)
+    {
+        var converterModel = this.PrepareConverterModel(group);
+        var json = JsonSerializer.Deserialize<UnitGroupCrawlerModel>(converterModel.Documents.GetValueOrDefault(1).Json);
+
+        foreach (var item in json.Units)
+        {
+            var code = this.GenerateCode("Summer", 2024, item.Id);
+            var dbUnit = await this.unitsService.GetAsync(x => x.Code == code);
+
+            if (dbUnit != null)
+            {
+                dbUnit.Number = string.IsNullOrEmpty(item.UnitNum) ? null : item.UnitNum;
+                dbUnit.LocationCode = item.Location;
+                dbUnit.LocationName = item.LocationDescription;
+                dbUnit.VenueCode = item.Venue;
+                dbUnit.VenueName = item.VenueDescription;
+                dbUnit.OlympicDay = DateTime.ParseExact(item.OlympicDay, "yyyy-MM-dd", null);
+                dbUnit.Session = item.SessionCode;
+                dbUnit.Group = item.GroupId;
+
+                this.unitsService.Update(dbUnit);
+            }
+        }
+    }
+}
