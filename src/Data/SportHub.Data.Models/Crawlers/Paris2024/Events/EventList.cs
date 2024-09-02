6d82192b4d911de83d7057a0b1033bde504a0ef6
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-09-02 09:53:02 +0300

    deleted models.

--- src/Data/SportHub.Data.Models/Crawlers/Paris2024/Events/EventList.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SportHub.Data.Models.Crawlers.Paris2024.Events;
-
-using System.Text.Json.Serialization;
-
-public class EventList
-{
-    [JsonPropertyName("events")]
-    public List<EventInfo> Events { get; set; }
-}
--- src/Data/SportHub.Data.Models/Crawlers/Paris2024/Events/EventUnitPhase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SportHub.Data.Models.Crawlers.Paris2024.Events;
-
-using System.Text.Json.Serialization;
-
-public class EventUnitPhase
-{
-    [JsonPropertyName("type")]
-    public string Type { get; set; }
-}
